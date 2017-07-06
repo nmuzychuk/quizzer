9bfa50d8952f71c00beb568cdda091d21ad65733
Nazarii Muzychuk <muzychuk.n@gmail.com>
2017-07-06 20:12:26 +0300

    Add db cleanup job

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :quizzes
+  has_many :quizzes, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
