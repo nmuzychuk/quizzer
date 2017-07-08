8c136ecfd85d0e09b54a67080e4ed9eb5839ddcc
Nazarii Muzychuk <muzychuk.n@gmail.com>
2017-07-08 22:41:28 +0300

    Update after log in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def after_sign_up_path_for(*)
+    dashboard_path
+  end
+
+  def after_sign_in_path_for(*)
+    dashboard_path
+  end
+
   private
 
   def authenticate_admin!
